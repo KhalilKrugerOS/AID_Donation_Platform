41b5e63c330964618ee1134bf3eb7d3d86779f8d
khalil lakhoua <86251644+KhalilKrugerOS@users.noreply.github.com>
2024-05-28 11:40:38 +0100

    minor changes + Licence fixed

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -58,8 +58,7 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
-    console.log(id);
-    console.log("from webhook...");
+
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
